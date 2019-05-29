3a78aba95502257aeb1b12fbabbaf44643c73df3
Sutou Kouhei <kou@clear-code.com>
2019-05-29 13:09:22 +0900

    travis: add

--- test/build.sh
+++ test/build.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
+if [ $# != 1 ]; then
+  echo "Usage: $0 CONTEXT"
+  echo " e.g.: $0 mysql57"
+  exit 1
+fi
+
 cd $(dirname $0)
+
+context=$1
 
 timestamp=$(date +%s)
 image_name="test_mroonga_${timestamp}"
 container_name="name_${image_name}"
 
-sudo docker build -t $image_name ../Dockerfile
+eval $(grep -E -o '[a-z]+_version=[0-9.]+' ../$context/Dockerfile)
+
+sudo docker build -t $image_name ../$context
 sudo docker run -d -p 33061:3306 --name $container_name $image_name
 ### Should test.
 while true ; do
-  /usr/mysql/5.6.43/bin/mysqladmin -h 127.0.0.1 -P 33061 -uroot ping && break
+  mysqladmin -h 127.0.0.1 -P 33061 -uroot ping && break
   sleep 1
 done
-mysql -h 127.0.0.1 -P 33061 -uroot -v -sse "SHOW VARIABLES LIKE '%version'"
+(echo -e "mroonga_libgroonga_version\t${groonga_version}"; \
+ echo -e "mroonga_version\t${mroonga_version}"; \
+ echo -e "version\t${mysql_version}") \
+  > /tmp/expected.txt
+mysql_e="mysql -h 127.0.0.1 -P 33061 -uroot -sse"
+($mysql_e "SHOW VARIABLES LIKE 'mroonga_libgroonga_version'"; \
+ $mysql_e "SHOW VARIABLES LIKE 'mroonga_version'"; \
+ $mysql_e "SHOW VARIABLES LIKE 'version'") \
+  > /tmp/actual.txt
+diff -u /tmp/expected.txt /tmp/actual.txt
+success=$?
 sudo docker stop $container_name
 sudo docker logs $container_name
 sudo docker rm $container_name
 sudo docker rmi $image_name
+exit $success
