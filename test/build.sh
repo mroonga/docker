81166adca2044f1c14f4f79067903e7bf6c8be64
Sutou Kouhei <kou@clear-code.com>
2021-06-02 13:40:20 +0900

    ci: detect MySQL version from FROM line

--- test/build.sh
+++ test/build.sh
@@ -17,6 +17,7 @@ image_name="test_mroonga_${timestamp}"
 container_name="name_${image_name}"
 
 eval $(grep -E -o '[a-z]+_version=[0-9.]+' ../$context/Dockerfile)
+mysql_version=$(head -n1 ../$context/Dockerfile | grep -E -o '[0-9.]+')
 
 sudo docker build -t $image_name ../$context
 sudo docker run \
