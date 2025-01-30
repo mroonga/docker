ec818f0530e75f723f7cb01a6108e38b27b4fb90
Horimoto Yasuhiro <horimoto@clear-code.com>
2025-01-30 11:29:20 +0900

    Get MySQL, Mroonga, and Groonga versions automatically in update.sh
    Usage: ./update.sh

--- update.sh
+++ update.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-set -eu
+set -eux
 
-if [ $# != 3 ]; then
-  echo "Usage: $0 MYSQL_VERSION MROONGA_VERSION GROONGA_VERSION"
-  echo " e.g.: $0 8.0.30 12.06 12.0.6"
+if [ $# != 0 ]; then
+  echo "Usage: $0"
   exit 1
 fi
 
-mysql_version=$1
-mroonga_version=$2
-groonga_version=$3
+target_mysqls=(
+  "8.0"
+  "8.4"
+)
+
+mroonga_version=$(curl https://api.github.com/repos/mroonga/mroonga/releases/latest \
+                    | jq -r '.["tag_name"]' | sed 's/^v//')
+groonga_version=$(curl https://api.github.com/repos/groonga/groonga/releases/latest \
+                    | jq -r '.["tag_name"]' | sed 's/^v//')
 
 if type gsed > /dev/null 2>&1; then
   SED=gsed
@@ -18,25 +23,38 @@ else
   SED=sed
 fi
 
-case $mysql_version in
-  8.0.*)
-    docker_file=mysql-8.0/Dockerfile
-    ;;
-esac
+for target_mysql in "${target_mysqls[@]}"; do
+  case $target_mysql in
+    8.0)
+      mysql_version=$(curl https://raw.githubusercontent.com/docker-library/mysql/refs/heads/master/versions.json \
+                        | jq -r '.["8.0"]["version"]')
+      docker_file=mysql-8.0/Dockerfile
+      ;;
+    8.4)
+      mysql_version=$(curl https://raw.githubusercontent.com/docker-library/mysql/refs/heads/master/versions.json \
+                        | jq -r '.["8.4"]["version"]')
+      docker_file=mysql-8.4/Dockerfile
+      ;;
+  esac
 
-${SED} \
-  -i'' \
-  -r \
-  -e "s/mysql:[0-9.]*/mysql:${mysql_version}/g" \
-  -e "s/mroonga_version=[0-9.]*/mroonga_version=${mroonga_version}/g" \
-  -e "s/groonga_version=[0-9.]*/groonga_version=${groonga_version}/g" \
-  ${docker_file}
-git add ${docker_file}
+  ${SED} \
+    -i'' \
+    -r \
+    -e "s/mysql:[0-9.]*/mysql:${mysql_version}/g" \
+    -e "s/mroonga_version=[0-9.]*/mroonga_version=${mroonga_version}/g" \
+    -e "s/groonga_version=[0-9.]*/groonga_version=${groonga_version}/g" \
+    ${docker_file}
+  git add ${docker_file}
 
-ruby "$(dirname "$0")/update-tag-list.rb" "$@"
-git add README.md
+  ruby "$(dirname "$0")/update-tag-list.rb" \
+       "${mysql_version}" \
+       "${mroonga_version}" \
+       "${groonga_version}"
+  git add README.md
 
-tag="mysql-${mysql_version}-${mroonga_version}"
-message="MySQL ${mysql_version} and Mroonga ${mroonga_version}"
-git commit -m "${message}"
-git tag -a -m "${message}" ${tag}
+  tag="mysql-${mysql_version}-${mroonga_version}"
+  message="MySQL ${mysql_version} and Mroonga ${mroonga_version}"
+  echo "${message}"
+  git commit -m "${message}"
+  git tag -a -m "${message}" ${tag}
+done
